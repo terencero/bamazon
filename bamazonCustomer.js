64322c20887167ab8e8901dfa9bc21737c979e92
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-08 13:32:17 -0600

    add chalk npm package; add promise to control product display and questionnaire flow:

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,27 +1,65 @@
 var inquirer = require('inquirer');
 
+var chalk = require('chalk');
+
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'nietzsche83!',
-	database: 'bamazon'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'nietzsche83!',
+    database: 'bamazon'
 });
 
 connection.connect(function(err) {
-	if (err) throw err;
-	
+    if (err) throw err;
 });
 
-connection.query('SELECT * FROM products', function(err, res) {
-	// 	for(i=0; i<res.length; i++){
-	// console.log(res[i].item_id + res[i].product_name + res[i].price);
-	// }
-	console.log(res);
-});
+inquirer.prompt([{
+        type: 'list',
+        message: 'Would you like to browse our selections?',
+        choices: ['yes', 'no'],
+        name: 'userChoice'
+    }
 
+]).then(function(ans) {
+    if (ans.userChoice === 'no') {
+        console.log('Sorry to see you go!  Come back soon to find great sustainable products for great prices!');
+
+    } else if (ans.userChoice === 'yes') {
+        new Promise(function(resolve, reject) {
+            connection.query('SELECT * FROM products', function(err, res) {
+
+                if (err) throw console.log('Oops...This is embarrassing... looks like our server is down. Try again later.');
+
+                for (i = 0; i < res.length; i++) {
+                    console.log(chalk.blue('Product Id:') + ' ' + chalk.blue(res[i].item_id) + ' ' + '|' + ' ' + chalk.red('Product Name:') + ' ' + chalk.red(res[i].product_name) + ' ' + '|' + ' ' + chalk.green('Price:') + ' ' + chalk.green(res[i].price));
+                }
+                resolve();
+            });
+
+        }).then(function(val1) {
+            inquirer.prompt([{
+                type: 'input',
+                message: 'Find something you like? Enter the item id to purchase.',
+                name: 'userItemId'
+            }, {
+                type: 'input',
+                message: 'How many would you like to purchase?',
+                name: 'userQuantity'
+            }]).then(function(ans) {
+                // check table; if statement: return success or insufficient quantities
+
+            });
+        }).catch(function(val2) {
+            console.log('Oops...This is embarrassing... looks like our server is down. Try again later.');
+        });
+    }
+
+}).then(function(val) {
+
+});
 // inquirer.prompt([
 
 // 	])
